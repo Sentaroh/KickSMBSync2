18d9f0d3707166c3195a7dc43f1ebfcd7af57ebe
sentaroh <gm.developer.fhoshino@gmail.com>
2018-02-18 06:16:57 +0900

    エラー目絵セージを表示するようにした

--- KickSMBSync2/src/main/java/com/sentaroh/android/KickSMBSync2/MainActivity.java
+++ KickSMBSync2/src/main/java/com/sentaroh/android/KickSMBSync2/MainActivity.java
@@ -2,17 +2,24 @@ package com.sentaroh.android.KickSMBSync2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
-        Intent in=new Intent("com.sentaroh.android.SMBSync2.ACTION_START_SYNC");
-        sendBroadcast(in,null);
-
+        final PackageManager pm = getPackageManager();
+        Intent inx=pm.getLaunchIntentForPackage("com.sentaroh.android.SMBSync2");
+        if (inx!=null) {
+            Intent in=new Intent("com.sentaroh.android.SMBSync2.ACTION_START_SYNC");
+            sendBroadcast(in,null);
+        } else {
+            //SMBSync2 was missing
+            Toast.makeText(this, getString(R.string.smbsync2_missing), Toast.LENGTH_LONG).show();
+        }
         finish();
     }
 }
