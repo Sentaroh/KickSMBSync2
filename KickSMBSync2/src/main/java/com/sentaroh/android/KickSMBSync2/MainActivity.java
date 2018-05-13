46c59d2d68fcf8cb7714aaf6a763bc40864a2d36
sentaroh <gm.developer.fhoshino@gmail.com>
2018-05-13 16:27:58 +0900

    Android 8 対応

--- KickSMBSync2/src/main/java/com/sentaroh/android/KickSMBSync2/MainActivity.java
+++ KickSMBSync2/src/main/java/com/sentaroh/android/KickSMBSync2/MainActivity.java
@@ -3,23 +3,35 @@ package com.sentaroh.android.KickSMBSync2;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_transrucent);
         final PackageManager pm = getPackageManager();
         Intent inx=pm.getLaunchIntentForPackage("com.sentaroh.android.SMBSync2");
         if (inx!=null) {
             Intent in=new Intent("com.sentaroh.android.SMBSync2.ACTION_START_SYNC");
-            sendBroadcast(in,null);
+            if (Build.VERSION.SDK_INT>=26) {
+                try {
+                    startActivity(in);
+                } catch(Exception e) {
+                    Toast.makeText(this, getString(R.string.smbsync2_ver_150_missing), Toast.LENGTH_LONG).show();
+                }
+            } else {
+                sendBroadcast(in,null);
+            }
         } else {
             //SMBSync2 was missing
             Toast.makeText(this, getString(R.string.smbsync2_missing), Toast.LENGTH_LONG).show();
         }
         finish();
     }
+
 }
